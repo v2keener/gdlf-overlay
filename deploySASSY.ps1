871c732d27abdc5f081229e0eee58ad9a0a002cf
Gustavo Keener <v2keener@gmail.com>
2023-02-27 10:37:29 -0500

    Updated deploySASSY.ps1 to *actually* use the first arg sent to it for a vault's .obsidian folder (work from yesterday)

--- deploySASSY.ps1
+++ deploySASSY.ps1
@@ -4,34 +4,49 @@
 
 # Note that all the following is using PowerShell (pwsh) on Windows 10/11
 
-# directory of $snippets should be `your-vault/.obsidian/snippets`
-#  It's the first parameter (e.g. `.\deploySASSY.ps1 'C:\your-vault\.obsidian\snippets')
-#  (The snippets folder is used as a relative path to themes as well)
-$snippets = $1
+# directory of $dot_obsidian_folder should be `your-vault/.obsidian`
+#  It's the first parameter (e.g. `.\deploySASSY.ps1 'C:\your-vault\.obsidian')
+#  (The .obsidian folder)
+$dot_obsidian_folder = $args[0]
 
-Write-Output "This will run some jobs"
-Write-Output ("CSS Snippets will be written to " + $snippets)
+# if(!(Test-Path $dot_obsidian_folder))
+# {
+#   Exit-PSHostProcess
+# }
+
+# echo $dot_obsidian_folder
+
+Write-Output "This will run some jobs."
+Write-Output ("CSS for the dot_obsidian_folder will be written to " + $dot_obsidian_folder)
 
 # Print settings (settings for PDF, printing)
-sass -w .\mixins\gdlf-media-print.scss "$snippets\gdlf-media-print.css" --no-source-map &
+sass -w .\mixins\gdlf-media-print.scss "$dot_obsidian_folder\snippets\gdlf-media-print.css" --no-source-map &
 
 # The SCSS files that combine all the mixins to be useful while 
 # containing all the Style Settings for each mixin in each of 
 # the two following files
-sass -w ".\gdlf-overlay.scss" "$snippets\gdlf-overlay.css" --no-source-map &
-sass -w ".\gdlf-for-me-wrapper.scss" "$snippets\gdlf-for-me.css" --no-source-map &
+sass -w ".\gdlf-overlay.scss" "$dot_obsidian_folder\snippets\gdlf-overlay.css" --no-source-map &
+sass -w ".\gdlf-for-me-wrapper.scss" "$dot_obsidian_folder\snippets\gdlf-for-me.css" --no-source-map &
 
 # Settings for other themes (Default, Prism)
 # From https://github.com/mgmeyers/obsidian-style-settings/blob/main/obsidian-default-theme.css
-# sass -w ".\mixins\obsidian-default-settings.scss" "$snippets\obsidian-default-settings.css" --no-source-map &
-# sass -w ".\mixins\gdlf-theme-prism-extras.scss" "$snippets\gdlf-theme-prism-extras.css" --no-source-map &
+# sass -w ".\mixins\obsidian-default-settings.scss" "$dot_obsidian_folder\snippets\obsidian-default-settings.css" --no-source-map &
+# sass -w ".\mixins\gdlf-theme-prism-extras.scss" "$dot_obsidian_folder\snippets\gdlf-theme-prism-extras.css" --no-source-map &
 
-sass -w ".\gdlf-overlay-combined.scss" "$snippets\gdlf-full-overlay.css" --no-source-map &
+sass -w ".\gdlf-overlay-combined.scss" "$dot_obsidian_folder\snippets\gdlf-full-overlay.css" --no-source-map &
 # For the next command to work, you need an "Overlay" folder under your "themes" directory...
-sass -w ".\gdlf-overlay-theme.scss" "$snippets\..\themes\Overlay\theme.css" --no-source-map &
+$overlay_path = ( Join-Path $dot_obsidian_folder "/themes/Overlay")
+# echo $overlay_path
+If(!(Test-Path -Path $overlay_path))
+{
+  # this is from https://stackoverflow.com/questions/16906170/create-directory-if-it-does-not-exist#46714857
+  New-Item -ItemType Directory -Path $overlay_path
+}
+sass -w ".\gdlf-overlay-theme.scss" "$dot_obsidian_folder\themes\Overlay\theme.css" --no-source-map &
+# Render to the root of project
 sass -w ".\gdlf-overlay-theme.scss" "theme.css" --no-source-map &
 
 # Now do plugins last
-sass -w ".\mixins\plugin-reminders.scss" "$snippets\plugin-reminders.css" --no-source-map &
-sass -w ".\mixins\plugin-grandfather.scss" "$snippets\plugin-grandfather.css" --no-source-map &
-sass -w ".\mixins\plugin-todo-list.scss" "$snippets\plugin-todo-list.css" --no-source-map &
+sass -w ".\mixins\plugin-reminders.scss" "$dot_obsidian_folder\snippets\plugin-reminders.css" --no-source-map &
+sass -w ".\mixins\plugin-grandfather.scss" "$dot_obsidian_folder\snippets\plugin-grandfather.css" --no-source-map &
+sass -w ".\mixins\plugin-todo-list.scss" "$dot_obsidian_folder\snippets\plugin-todo-list.css" --no-source-map &
